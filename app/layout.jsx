0969276385754f2010cf1e80562563f7a0492604
jangdeaho <tunawagu@gmail.com>
2026-02-22 08:47:41 +0900

    22

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,13 +31,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <head>
-        {/* Pretendard 폰트 */}
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
